47d7fbf9c51f8f4d42ed1f36189d0fa6ca519ef1
Unknown <svenjanssen12321@gmail.com>
2019-08-05 17:39:10 +0200

    Added background music

---
+++ Gamejam/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public float WaitTime;
+
+    private AudioSource Audio;
+
+    // Use this for initialization
+    void Start()
+    {
+        Audio = GetComponent<AudioSource>();
+
+        Health.OnPlayerDeath += StopMusic;
+        Field.OnFirstKill += StartMusic;
+    }
+
+    IEnumerator _Wait(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Audio.Play();
+    }
+
+    private void StartMusic()
+    {
+        StartCoroutine(_Wait(WaitTime));
+    }
+
+    private void StopMusic()
+    {
+        Audio.Stop();
+    }
+
+    private void OnDestroy()
+    {
+        Health.OnPlayerDeath -= StopMusic;
+        Field.OnFirstKill -= StartMusic;
+    }
+}
